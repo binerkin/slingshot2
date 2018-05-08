8f6e42d150b292771b177b6c83944d26af370874
Andreas Lindgren <Andreas.Lindgren@alten.se>
2018-05-08 12:50:20 +0200

    untracked files

---
+++ slingshot2/slingshot2/slingshot2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace slingshot2
+{
+    public partial class slingshot2Page : ContentPage
+    {
+        public slingshot2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
